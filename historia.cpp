8b970ec8eec43edd8462bb212eaf0dd001eae54d
Igor <136636456+IgorXF@users.noreply.github.com>
2023-06-14 15:37:23 -0300

    Add files via upload

---
+++ historia.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"A luz no fim do codigo! (:\n\n"<< endl;
+
+    cout<<"Bem vindo(a) ao jogo, nesse game voce ira aprender um pouco mais "<<endl;
+    cout<<"do mundo da computacao e suas possivel areas de atuacao. Ao final do quiz"<<endl; 
+    cout<<"nos iremos direciona-los para uma respectiva area que acreditamos que"<<endl;
+    cout<<"combine com voce. (:\n"<<endl;;
+
+    cout<<"Primeiramente, voce precisa saber, que a computacao pode estar presente"<<endl;
+    cout<<"em todas as areas, sendo aplicada de diversas formas.Como por exemplo no"<<endl;
+    cout<<"agronegocio com a automacao em maquinas, uso de drones, sistema de irrigacao,"<<endl;
+    cout<<"entre outros; esta presente tambem em outras areas, como medicina, ensino,"<<endl;
+    cout<<"e em todo lugar que voce possa imaginar. Temos como principal objetivo, trazer"<<endl;
+    cout<<"a otimizacao de tarefas, praticidade e rapidez.</> \n"<<endl;
+    
+    int opcao = 0;
+
+    cout<<"Vamos comecar!!!Escolha uma area a qual pretende conhecer.\n"<<endl;
+    cout<<"(Vale ressaltar que a computacao nao se limita as areas que vamos abordar aqui!) (:\n"<<endl;
+
+    cout<<"------------------------------------------------------------------------- "<<endl;
+    cout<<"|\t\t1_\t>>INTELIGENCIA ARTIFICIAL<<\t\t\t|"<<endl;
+    cout<<"|\t\t2_\t>>COMPUTACAO GRAFICA<<\t\t\t\t|"<<endl;
+    cout<<"|\t\t3_\t>>ENGENHARIA DE SOFTWARE<<\t\t\t|"<<endl;
+    cout<<"|\t\t4_\t>>SEGURANCA DA INFORMACAO<<\t\t\t|"<<endl;
+    cout<<"|\t\t5_\t>>ARQUITETURA DE COMPUTADORES<<\t\t\t|"<<endl;
+    cout<<"------------------------------------------------------------------------- "<<endl;
+
+    
+
+    return 0;
+}
